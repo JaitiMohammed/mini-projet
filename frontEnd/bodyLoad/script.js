e78cd1b7ad52a87cd25f029dd7cb0b965c7a1974
Jaiti Mohammed <38511742+JaitiMohammed@users.noreply.github.com>
2019-11-09 18:20:27 +0100

    new commit for 9 nov 19

---
+++ frontEnd/bodyLoad/script.js
@@ -0,0 +1,11 @@
+var overlay = document.getElementById("overlay");
+var img = document.querySelector('img');
+
+
+window.addEventListener('load', function(){
+    
+       // overlay.style.display = 'none';
+       // img.style.display='none';
+    
+
+});
